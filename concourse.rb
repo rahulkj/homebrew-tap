774c39303015e1bdaead9f0f53a9c7fc69e4ff80
Rahul Jain <rahulkj@gmail.com>
2019-06-03 21:18:49 -0500

    Fixing om version

--- concourse.rb
+++ concourse.rb
@@ -3,7 +3,7 @@ class Concourse < Formula
   homepage "https://github.com/pivotal-cf/om"
   version  "5.2.0"
   url      "https://github.com/concourse/concourse/releases/download/v#{version}/concourse-#{version}-darwin-amd64.tgz"
-  sha256   "91e1d86621165fda49eb9c535d3e70fae3bb4cac"
+  sha256   "d0f9e00c47936ca88c439872f76662f625510a5ed8838d758e18ac214e2c4ec4"
 
   depends_on :arch => :x86_64
 
