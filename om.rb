340fdb3147218cf6257ef428a0d4f59cbb8357e9
Rahul Jain <rahulkj@gmail.com>
2019-06-03 21:06:35 -0500

    Fixing om version

--- om.rb
+++ om.rb
@@ -1,9 +1,9 @@
 class Om < Formula
   desc     "Pivotal - om cli"
   homepage "https://github.com/pivotal-cf/om"
-  version  "v1.1.0"
+  version  "1.1.0"
   url      "https://github.com/pivotal-cf/om/releases/download/#{version}/om-darwin"
-  sha256   "f3832d8966dd39f7ae1316195ebb379cf18aece281bc2f7c43dd799130ebf460"
+  sha256   "69c4bc3b538fbdb83c5fd6a7bbf2c999293e28d8b895e9d09cd4a7211bce272c"
 
   depends_on :arch => :x86_64
 
