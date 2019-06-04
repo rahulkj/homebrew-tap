c78829fb32a91db7c674ebeb2fe509f2a92c3e78
Rahul Jain <rahulkj@gmail.com>
2019-06-04 11:55:17 -0500

    Adding readme

---
+++ README.md
@@ -0,0 +1,11 @@
+HOMEBREW TAP
+---
+
+This tap provides formulas to install the following CLI's:
+
+| CLI | Link |
+| --- | --- |
+| Cred Alert | https://github.com/pivotal-cf/cred-alert |
+| Concourse | https://github.com/concourse/concourse |
+| Fly | https://github.com/concourse/concourse |
+| OM | https://github.com/pivotal-cf/om |
--- cred-alert.rb
+++ cred-alert.rb
@@ -1,6 +1,6 @@
 class CredAlert < Formula
   desc     "Pivotal - cred-alert cli"
-  homepage "https://github.com/concourse/concourse"
+  homepage "https://github.com/pivotal-cf/cred-alert"
   version  "2019-03-28-22-27-36"
   url      "https://github.com/pivotal-cf/cred-alert/releases/download/#{version}/cred-alert-cli_darwin"
   sha256   "9cfadb138e4ed610be9c3bdd3b1a4f5e72c483f1797b16c92b6dc0d25ffab004"
