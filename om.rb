284aa237c63ffd582885906f5824fe10007d3a7f
Rahul Jain <rahulkj@gmail.com>
2019-07-09 16:53:01 -0400

    Updating om function

--- om.rb
+++ om.rb
@@ -2,13 +2,13 @@ class Om < Formula
   desc     "Pivotal - om cli"
   homepage "https://github.com/pivotal-cf/om"
   version  "2.0.1"
-  url      "https://github.com/pivotal-cf/om/releases/download/#{version}/om-darwin"
+  url      "https://github.com/pivotal-cf/om/releases/download/#{version}/om-darwin-#{version}.tar.gz"
   sha256   "578235fed7ce64a16d2883f98bc91dbc63aafde1c1bf59664c6fb5edc3814b47"
 
   depends_on :arch => :x86_64
 
   def install
-    bin.install "om-darwin" => "om"
+    bin.install "om"
   end
 
   test do
