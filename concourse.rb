144e6b594a658295140371466c2cf17b638c71e4
rahulkj <rahulkj@gmail.com>
2019-08-27 03:44:18 +0000

    Updated Formulas

--- concourse.rb
+++ concourse.rb
@@ -1,9 +1,9 @@
 class Concourse < Formula
   desc     "Concourse Product - concourse cli"
   homepage "https://github.com/concourse/concourse"
-  version  "5.2.1"
+  version  "5.4.1"
   url      "https://github.com/concourse/concourse/releases/download/v#{version}/concourse-#{version}-darwin-amd64.tgz"
-  sha256   "53185a6df0682550dbbb9ef08cf0b713f84045f827d3396c70055d50446894c2"
+  sha256   "ca5eaa16bb7e875edd65849ad821ea428a55508b3a53da46fe80ab24549df217"
 
   depends_on :arch => :x86_64
 
