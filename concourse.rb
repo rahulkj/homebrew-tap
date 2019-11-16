1e2d380a84dbcd81771882637dad473ebaf932f9
rahulkj <rahulkj@gmail.com>
2019-11-16 03:18:51 +0000

    Updated Formulas

--- concourse.rb
+++ concourse.rb
@@ -1,9 +1,9 @@
 class Concourse < Formula
   desc     "Concourse Product - concourse cli"
   homepage "https://github.com/concourse/concourse"
-  version  "5.5.5"
+  version  "5.5.6"
   url      "https://github.com/concourse/concourse/releases/download/v#{version}/concourse-#{version}-darwin-amd64.tgz"
-  sha256   "8f22a5d36fab1d7d24e1fe63cdb61a02bd1d2e805a17e8fafd2490d29900c12a"
+  sha256   "4f313d5fcf8f5a55f29a71ff5f6f2d92bd49a6d9a01059fb243c2c7297f52d25"
 
   depends_on :arch => :x86_64
 
--- fly.rb
+++ fly.rb
@@ -1,9 +1,9 @@
 class Fly < Formula
   desc     "Concourse Product - fly cli"
   homepage "https://github.com/concourse/concourse"
-  version  "5.5.5"
+  version  "5.5.6"
   url      "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  sha256   "b1ddb84d5e02efe824d0f716b5e70daf41d9cf851aede755cbd563de54860d55"
+  sha256   "1589ec09a38e6c38ff5f1275b21fb1732e3586a208aed9586fe9ebd51f913db6"
 
   depends_on :arch => :x86_64
 
