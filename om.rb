9f2d06cd9cd4886bc65b635dfeaf5b85da441c03
rahulkj <rahulkj@gmail.com>
2020-02-13 21:40:59 +0000

    Updated Formulas

--- om.rb
+++ om.rb
@@ -1,9 +1,9 @@
 class Om < Formula
   desc     "Pivotal - om cli"
   homepage "https://github.com/pivotal-cf/om"
-  version  "4.4.2"
+  version  "4.5.0"
   url      "https://github.com/pivotal-cf/om/releases/download/#{version}/om-darwin-#{version}.tar.gz"
-  sha256   "38497a6048c4ca26fb06fbf43fa9115975b27a4b5435b10dfca3cbd00ba7811e"
+  sha256   "5f95ec2e1dd8a2f76b4f553bf41cf7cebc7deab2dbf799db0324f0aa48d1fc04"
 
   depends_on :arch => :x86_64
 
