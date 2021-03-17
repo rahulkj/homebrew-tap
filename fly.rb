ea32448b806543197007f9eb9c75a977dac040e4
rahulkj <rahulkj@gmail.com>
2021-03-17 14:40:10 +0000

    Updated Formulas

--- fly.rb
+++ fly.rb
@@ -1,9 +1,9 @@
 class Fly < Formula
   desc     "Concourse Product - fly cli"
   homepage "https://github.com/concourse/concourse"
-  version  "7.0.0"
+  version  "7.1.0"
   url      "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  sha256   "7cdbf924d8f4e18a6f7a6bef65edc25f5ac234161aca21c9f24ed18b337d19db"
+  sha256   "461fb517430bd1d8f21fe0e2f59d4c293770d7c76e64d7c96133d5c63e3e04b5"
 
   depends_on :arch => :x86_64
 
