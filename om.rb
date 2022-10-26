0a91e3747baa01a928c012f8db646ba8eabdba50
Rahul Jain <rahulkj@gmail.com>
2022-10-25 22:18:18 -0500

    Updating om file name

--- om.rb
+++ om.rb
@@ -3,7 +3,7 @@ class Om < Formula
   homepage "https://github.com/pivotal-cf/om"
   version  "7.8.2"
   url      "https://github.com/pivotal-cf/om/releases/download/#{version}/om-darwin-amd64-#{version}.tar.gz"
-  sha256   "c6a855d8a5ba2eb8c13b796419eee8d886c3c25db76f9b05394b5117a2f9b971"
+  sha256   "4c3cefa82ba21b7535cfe58c1588a5f0e59ff85ee789469ee25acf05aef87fbd"
 
   depends_on :arch => :x86_64
 
